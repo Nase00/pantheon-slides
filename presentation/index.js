596a579c0de40a56798a3d77875cbe0f16346410
sowiecki <nase00@gmail.com>
2018-01-28 02:04:20 -0600

    Initial commit on top of boilerplate

--- presentation/index.js
+++ presentation/index.js
@@ -1,71 +1,110 @@
-// Import React
 import React from "react";
-
-// Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from "spectacle";
-
-// Import theme
+import CodeSlide from "spectacle-code-slide";
+import { Deck, Heading, ListItem, List, Slide, Text } from "spectacle";
 import createTheme from "spectacle/lib/themes/default";
 
-// Require CSS
+import spaghettiBath from "../assets/spaghetti-bath.jpg";
+import pantheonPre from "../assets/pantheon-pre.png";
+import pantheonPost from "../assets/pantheon-post.png";
+
 require("normalize.css");
 
-const theme = createTheme({
-  primary: "white",
-  secondary: "#1F2022",
-  tertiary: "#03A9FC",
-  quarternary: "#CECECE"
-}, {
-  primary: "Montserrat",
-  secondary: "Helvetica"
-});
+const theme = createTheme(
+  {
+    primary: "#1F2022",
+    secondary: "white",
+    tertiary: "#05a8fa",
+    quarternary: "#CECECE"
+  },
+  {
+    primary: "Montserrat",
+    secondary: "Helvetica"
+  }
+);
 
 export default class Presentation extends React.Component {
   render() {
     return (
-      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+      <Deck
+        transition={["zoom", "slide"]}
+        transitionDuration={500}
+        theme={theme}
+      >
         <Slide transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
+            Pantheon
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+            Universal IoT Integration
+          </Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
+          <Heading size={5} textColor="secondary" caps>
+            Some Common Communication Protocols
+          </Heading>
+          <List>
+            <ListItem>WiFi</ListItem>
+            <ListItem>Bluetooth</ListItem>
+            <ListItem>WebSocket</ListItem>
+            <ListItem>ZigBee</ListItem>
+            <ListItem>HTTP (Hyper Text Transfer Protocol)</ListItem>
+            <ListItem>MQTT (Message Queuing Telemetry Transport)</ListItem>
+            <ListItem>AMQP (Advanced Message Queueing Protocol</ListItem>
+          </List>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={5} textColor="secondary" caps>
+            Direct connections between devices and services
+          </Heading>
+          <img src={pantheonPre} />
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary">
+          <Heading size={5} textColor="secondary" caps>
+            Feels like
+          </Heading>
+          <img src={spaghettiBath} style={{ borderRadius: "60px" }} />
+          <Text size={2} textColor="secondary" style={{ fontSize: "8px" }}>
+            (credit: Google Images)
           </Text>
         </Slide>
         <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+          <Heading size={5} textColor="secondary" caps>
+            A saner method
+          </Heading>
+          <img src={pantheonPost} />
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={5} textColor="secondary" caps>
+            Service integrations
+          </Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>Spotify OAuth</ListItem>
+            <ListItem>Philips HUE</ListItem>
+            <ListItem>Unified Remote</ListItem>
+            <ListItem>Particle Cloud</ListItem>
           </List>
         </Slide>
-        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
+        <CodeSlide
+          transition={["fade"]}
+          lang="js"
+          code={require("raw-loader!../assets/event.example")}
+          ranges={[
+            { loc: [0, 270], title: "Example event sequence" },
+            { loc: [1, 2], note: "Restrict guest access" },
+            { loc: [2, 6], note: "Set custom state" },
+            { loc: [7, 14], note: "Set custom state" }
+          ]}
+        />
+        <CodeSlide
+          transition={["fade"]}
+          lang="json"
+          code={require("raw-loader!../assets/config.example")}
+          ranges={[
+            { loc: [0, 270], title: "Tenant configuration" },
+            { loc: [2, 10], note: "Restrict guest access" },
+            { loc: [27, 39], note: "Set custom state" }
+          ]}
+        />
       </Deck>
     );
   }
